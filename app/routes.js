b5f80698070eea72175d7db22cbee84c1848f0f7
Victor Timpau <timpauvictor@gmail.com>
2016-03-08 10:47:12 -0500

    renamed some functions, added required function call when making a point from the body

--- app/routes.js
+++ app/routes.js
@@ -5,17 +5,17 @@ module.exports = function(app) {
 	//server routes to handle api calls
 	//authentication routes=====================================
 	app.get('/api/points', function(req, res) {
-		Point.find(function(err, points) {
+		Point.find(function(err, gotPoints) {
 			if (err) {
 				res.send(err);
 			}
-			res.json(points);
+			res.json(gotPoints);
 		});
 	});
 
 	app.post('/api/addPoint', function(req, res) {
-		consle.log(req.body);
-		var newPoint = req.body;
+		console.log(req.body);
+		var newPoint = Point(req.body);
 		newPoint.save(function(err) {
 			if (err) throw (err);
 			console.log("New Point Made");

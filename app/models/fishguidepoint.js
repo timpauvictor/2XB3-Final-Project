b2d9742cb95099649b43d2766339e444f2b2f73c
Victor Timpau <timpauvictor@gmail.com>
2016-03-13 13:00:03 -0400

    updated fish model

--- app/models/fishguidepoint.js
+++ app/models/fishguidepoint.js
@@ -4,23 +4,25 @@ var mongoose = require('mongoose'); //make a new mongoose object
 //defining the model for a point in our databse
 module.exports = mongoose.model('fishGuidePoint', {
 	waterBodyCode: {type: Number, default: 0},
-	locName_ENG: {type: String, default: ' '},
-	locName_FRE: {type: String, default: ' '},
-	specName_ENG: {type: String, default: ' '},
-	specName_FRE: {type: String, default: ' '},
-	populationTypeID: {type: String, default: ' '},
-	populationTypeDESC: {type: String, default: ' '},
-	lengthCategoryID: [],
-	lengthCategoryLbl: [],
-	advLevel: {type: Number, default: ' '},
-	advCauseID: {type: Number, default: ' '},
-	advCauseDESC: {type: String, default: ' '},
-	guideYear: {type: Number, default: ' '},
-	guideLocDescription: {type: String, default: ' '},
+	locName: {type: String, default: ' '},
 	geometry: {
 		lat: {type: Number, default: 0},
-		lng: {type: Number, default: 0}
+		lng: {type: Number, default: 0},
 	}
+	species: [{
+		code: {type: Number, default: 0},
+		name: {type: String, default: ' '},
+		lengths: [{
+			id: {type: String, default: ' '},
+			label: {type: String, default: ' '},
+			advLevel: {type: Number, default: ' '},
+			advCause: {type: Number, default: ' '},
+			advCauseDesc: {type: String, default: 0},
+			popTypeID: {type: Number, default: 0},
+			popTypeDESC: {type: String, default: 0}
+		}]
+	}]
+	locDESC: {type: String, default: ' '}
 	//defining anything else happens here, make sure you add a comma
 	//to the previous line
 });

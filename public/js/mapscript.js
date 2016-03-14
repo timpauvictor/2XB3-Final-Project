1daef6d8f0a6a3f09e76a8c88394318ea21f8f89
Victor Timpau <timpauvictor@gmail.com>
2016-03-14 12:45:24 -0400

    sucessfully getting points in get request

--- public/js/mapscript.js
+++ public/js/mapscript.js
@@ -1,15 +1,26 @@
-__startingCoords = [0, 0]
-__zoomLevel = 4;
+var __startingCoords = [0, 0]
+var __zoomLevel = 4;
+var allPoints;
 
 function loadMap() {
     __map = L.map("map").setView(__startingCoords, __zoomLevel);
     console.log('map created');
     L.esri.basemapLayer("Topographic").addTo(__map);
     console.log('topographic layer added');
+    __map.invalidateSize();
 }
 
 function getPoints() {
-	
+	console.log("Attempting to get all points");
+	var jQueryPromise = $.get('http://localhost:8080/api/points', {
+		dataType: "jsonp"
+	});
+	var realPromise = Promise.resolve(jQueryPromise);
+	realPromise.then(function(val) {
+		allPoints = val;
+	})
 }
 
 loadMap();
+__map.invalidateSize();
+getPoints();

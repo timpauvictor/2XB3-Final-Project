cfdc1e429b581afed358e019c919738d1ea63df3
Victor Timpau <timpauvictor@gmail.com>
2016-03-14 13:03:30 -0400

    displaying all points received

--- public/js/mapscript.js
+++ public/js/mapscript.js
@@ -1,7 +1,7 @@
 var __startingCoords = [0, 0]
 var __zoomLevel = 4;
-var allPoints;
 var __map;
+var allMarkers = [];
 
 function loadMap() {
     __map = L.map("map").setView(__startingCoords, __zoomLevel);
@@ -13,15 +13,29 @@ function loadMap() {
 
 function getPoints() {
 	console.log("Attempting to get all points");
-	var jQueryPromise = $.get('http://localhost:8080/api/points', {
+	var jQueryPromise = $.get('http://localhost:8080/api/fishPoints', {
 		dataType: "jsonp"
 	});
 	var realPromise = Promise.resolve(jQueryPromise);
 	realPromise.then(function(val) {
-		allPoints = val;
+		plotPoints(val);
 	})
+}
+
+function plotPoints(points) {
+	console.log(points);
+	for (var i = 0; i < points.length; i++) {
+		console.log(points[i].geometry.lat, points[i].geometry.lng);
+		var marker = L.marker([points[i].geometry.lat, points[i].geometry.lng]).addTo(__map);
+		popupString = makeString(points[i]);
+	}
+}
+
+function makeString(point) {
+	
 }
 
 loadMap();
 __map.invalidateSize();
 getPoints();
+__map.invalidateSize();

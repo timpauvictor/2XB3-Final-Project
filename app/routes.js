652c900faa0253735876547afcc2df8f21c62b71
Victor Timpau <timpauvictor@gmail.com>
2016-04-10 21:07:26 -0400

    added requirements to graph and shortest path and sorting
    also added routes for the above

--- app/routes.js
+++ app/routes.js
@@ -1,8 +1,54 @@
 var fishPoint = require('./models/fishguidepoint.js');
 var waterPoint = require('./models/waterpoint.js');
-//var diGraph = require('./models/Digraph.js');
+var diGraph = require('./Digraph.js');
+var sp = require('./DjikstraSP.js');
+var sort = require('./sort.js');
 
 module.exports = function(app) {
+
+	// app.get('/api/getShortestPath', function(req, res) {
+	// 	console.log()
+	// }),
+
+	app.get('/api/sortListData', function(req, res) {
+
+		console.log("Received request to sort all points");
+		startTime = Date.now();
+		var resData = [{}]
+		fishPoint.find(function(err, resPoint) {
+			if (err) {
+				res.send(err);
+			}
+
+			for (var i = 0; i < resPoint.length; i++) {
+				resData[i] = {
+					"code": parseInt(resPoint[i].waterBodyCode),
+					"name": resPoint[i].locName,
+				}
+				var specs = [];
+				for (var j = 0; j < resPoint[i].species.length; j++) {
+					specs.push(resPoint[i].species[j].name);
+				}
+				resData[i]["specs"] = specs;
+			}
+			checkpoint = Date.now();
+			console.log("Finished making data from database in", Date.now() - startTime, "ms");
+
+
+			//-------------------Quicksort
+			sortedData = sort.quickSort(resData)
+			console.log("Finished sorting in", Date.now() - checkpoint, "ms");
+			console.log("Checking for correctness...");
+			for (var i = 0; i < sortedData.length - 1; i++) {
+				if (sortedData[i+1] > sortedData[i]) {
+					console.log("Failed check");
+				}
+			}
+			console.log("Passed check and sending to user")
+			res.send(sortedData);
+
+		});
+	}),
 
 	//server routes to handle api calls
 	app.get('/api/fishPoints', function(req, res) {
@@ -37,7 +83,8 @@ module.exports = function(app) {
 			for (var i = 0; i < resPoint.length; i++) {
 				if ((Number(req.body.lat) === resPoint[i].geometry.lat) && (Number(req.body.lng) === resPoint[i].geometry.lng)) {
 					console.log("Match found");
-					res.send(resPoint[i]);					
+					res.send(resPoint[i]);
+					break;				
 				}
 			}	
 		});
@@ -70,17 +117,6 @@ module.exports = function(app) {
 		})
 		console.log('Request fulfilled');
 	});
-	
-/* 	app.post('/api/diGraph', function(req, res) {
-		console.log("Recieved request to add a Graph")
-		var newdiGraph = diGraph(req.body);
-		newdiGraph.save(function(err) {
-			if (err) throw (err);
-			console.log('New diGraph made');
-			res.send('Success');
-		})
-		console.log('Request fulfilled');
-	}); */
 
 	app.get('/api/waterPoints', function (req, res) {
 		console.log("Received request for all waterPoints");
